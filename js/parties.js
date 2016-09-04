89cdd93fbdb71789a7aece5263aeab004d86f7d9
James Alexander <jalexander@leafsoftwaresolutions.com>
2016-09-04 08:06:03 -0400

    - Converting all static HTML into JSON data - Adding Mustache templates to render JSON into HTML

---
+++ js/parties.js
@@ -0,0 +1,200 @@
+var partySchedule = {};
+
+partySchedule.thursdayParties = [
+    {
+        month: 'September',
+        year: 2016,
+        rows: [
+            {
+                days: [
+                    {
+                        day: 1,
+                        dances: ['Tango', 'Hustle', 'Foxtrot']
+                    },
+                    {
+                        day: 8,
+                        dances: ['Rumba', 'V-Waltz', 'WC Swing']
+                    },
+                    {
+                        day: 15,
+                        dances: ['Waltz', 'Cha Cha', 'Bolero']
+                    },
+                ]
+            },
+            {
+                days: [
+                    {
+                        day: 22,
+                        dances: ['Tango', 'Hustle', 'Foxtrot']
+                    },
+                    {
+                        day: 27,
+                        message: 'Jen\'s Birthday',
+                        dances: ['Other birthdays: Meatloaf (the singer) & Google'],
+                        class: 'bday'
+                    },
+                    {
+                        day: 29,
+                        dances: ['No scheduled dances'],
+                        class: 'event'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        month: 'October',
+        year: 2016,
+        rows: [
+            {
+                days: [
+                    {
+                        day: 6,
+                        dances: ['Rumba', 'V-Waltz', 'WC Swing']
+                    },
+                    {
+                        day: 13,
+                        message: 'Showcase Dress Rehearsal',
+                        dances: ['No scheduled dances']
+                    },
+                    {
+                        day: 20,
+                        dances: ['Waltz', 'Cha Cha', 'Bolero']
+                    },
+                ]
+            },
+
+            {
+                days: [
+                    {
+                        day: 27,
+                        dances: ['Tango', 'Hustle', 'Foxtrot']
+                    },
+                    {
+                        day: 31,
+                        message: 'Happy Halloween!',
+                        class: "holiday"
+                    }
+                ]
+            }
+
+        ]
+    },
+    {
+        month: 'November',
+        year: 2016,
+        rows: [
+            {
+                days: [
+                    {
+                        day: 3,
+                        message: 'TOC Kick Off Party',
+                        dances: ['Rumba', 'V-Waltz', 'WC Swing'],
+                        class: 'event'
+                    },
+                    {
+                        day: 8,
+                        message: 'Nicole\'s Birthday!',
+                        class: 'bday',
+                        dances: ['Other Birthdays: Vlad the Impaler & Bram Stoker (Coincidence?)']
+                    },
+                    {
+                        day: 10,
+                        dances: ['Waltz', 'Cha Cha', 'Bolero']
+                    }
+                ]
+            },
+            {
+                days: [
+                    {
+                        day: 17,
+                        dances: ['Tango', 'Hustle', 'Foxtrot']
+                    },
+                    {
+                        day: 24,
+                        message: 'Happy Thanksgiving!',
+                        class: 'holiday'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        month: "December",
+        year: 2016,
+        rows: [
+            {
+                days: [
+                    {
+                        day: 1,
+                        dances: ['Rumba', 'V-Waltz', 'WC Swing']
+                    },
+                    {
+                        day: 8,
+                        message: 'Medal Ball Christmas Party',
+                        dances: ['No scheduled dances'],
+                        class: 'event'
+                    },
+                    {
+                        day: 15,
+                        message: 'Victory Party & Auction',
+                        dances: ['Waltz', 'Cha Cha', 'Bolero'],
+                        class: 'event'
+                    }
+                ]
+            },
+            {
+                days: [
+                    {
+                        day: 22,
+                        dances: ['Tango', 'Hustle', 'Foxtrot']
+                    },
+                    {
+                        day: 25,
+                        message: 'Merry Christmas!',
+                        class: 'holiday'
+                    },
+                    {
+                        day: 29,
+                        dances: ['Rumba', 'V-Waltz', 'WC Swing']
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        month: "January",
+        year: 2017,
+        rows: [
+            {
+                days: [
+                    {
+                        day: 1,
+                        message: 'Happy New Year!',
+                        class: 'holiday'
+                    },
+                    {
+                        day: 5,
+                        dances: ['Waltz', 'Cha Cha', 'Bolero']
+                    },
+                    {
+                        day: 12,
+                        dances: ['Tango', 'Hustle', 'Foxtrot']
+                    }
+                ]
+            },
+            {
+                days: [
+                    {
+                        day: 19,
+                        dances: ['Rumba', 'V-Waltz', 'WC Swing']
+                    },
+                    {
+                        day: 26,
+                        dances: ['Waltz', 'Cha Cha', 'Bolero']
+                    }
+                ]
+            }
+        ]
+    }
+]
---
+++ js/schedule.js
@@ -0,0 +1,98 @@
+(function () {
+
+    function getMonthNumber(monthName) {
+        var d = new Date();
+        var month = new Array();
+        month["January"] = 0;
+        month["February"] = 1;
+        month["March"] = 2;
+        month["April"] = 3;
+        month["May"] = 4;
+        month["June"] = 5;
+        month["July"] = 6;
+        month["August"] = 7;
+        month["September"] = 8;
+        month["October"] = 9;
+        month["November"] = 10;
+        month["December"] = 11;
+
+        return month[monthName];
+    }
+
+    function getSuffixForDay(i) {
+        var j = i % 10,
+            k = i % 100;
+        if (j == 1 && k != 11) {
+            return "st";
+        }
+        if (j == 2 && k != 12) {
+            return "nd";
+        }
+        if (j == 3 && k != 13) {
+            return "rd";
+        }
+        return "th";
+    }
+
+    function populateWeekDays(parties) {
+        var weekday = new Array(7);
+        weekday[0] = "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
+
+        for (var i = 0; i < parties.length; i++) {
+            console.log(parties[i]);
+            var month = getMonthNumber(parties[i].month);
+            for (var j = 0; j < parties[i].rows.length; j++) {
+                days = parties[i].rows[j].days;
+                for (var k = 0; k < days.length; k++) {
+                    var d = new Date(parties[i].year, month, days[k].day);
+                    days[k].weekDay = weekday[d.getDay()];
+                    days[k].daySuffix = getSuffixForDay(days[k].day);
+                }
+            }
+        }
+    }
+
+    function removePastDays(parties) {
+        var newParties = [];
+        for (var i = 0; i < parties.length; i++) {
+            newParties.push({});
+            newParties[i].month = parties[i].month;
+            newParties[i].year = parties[i].year;
+            newParties[i].rows = [];
+            var month = getMonthNumber(parties[i].month);            
+            
+            for (var j = 0; j < parties[i].rows.length; j++) {
+                newParties[i].rows.push({});
+                newParties[i].rows[j].days = [];
+                days = parties[i].rows[j].days;
+                
+                for (var k = 0; k < days.length; k++) {
+                    var d = new Date(parties[i].year, month, days[k].day);
+                    if (d > new Date()) {
+                        newParties[i].rows[j].days.push({
+                            day: days[k].day,
+                            message: days[k].message,
+                            dances: days[k].dances,
+                            class: days[k].class
+                        })
+                    }
+                }
+            }
+        }
+
+        return newParties;
+    }
+
+    partySchedule.thursdayParties = removePastDays(partySchedule.thursdayParties);
+    populateWeekDays(partySchedule.thursdayParties);
+    var template = $('#template').html();
+    Mustache.parse(template);   // optional, speeds up future uses
+    var rendered = Mustache.render(template, { parties: partySchedule.thursdayParties });
+    $('#mainTemplate').html(rendered);
+})();
